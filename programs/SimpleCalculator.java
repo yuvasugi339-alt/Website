c08c40476fea66217a6a43b2131a033efce0b5a0
yuvasugi339-alt <yuvasugi339@gmail.com>
2026-01-30 18:23:33 +0000

    Add programs folder and load Java files from GitHub API

---
+++ programs/SimpleCalculator.java
@@ -0,0 +1,11 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
